86c27b04c0f5c38a201a38865ab8bcf8a13184d8
Fabrizio Manna <mannafabrizio83@gmail.com>
2018-01-18 14:19:25 +0100

    Add files via upload

---
+++ js/MainJs.js
@@ -0,0 +1,25 @@
+/*
+
+	JAVASCRIPT FILE DI GESTIONE AVVIO 
+	
+*/
+
+/*----INIZZIALIZZARE GRANDEZZE ALL'AVVIO----*/
+
+window.onload = function(){
+	
+	/*Inizializzare-mappa*/
+	
+		var mappa = document.getElementById("mappa");
+	
+	/*settaggio parametri mappa*/
+		
+		mappa.style.width = objmappa.width + "px";
+		mappa.style.height = objmappa.height + "px";
+	
+	/*caricamento-sprite*/
+		var sprite = document.getElementById("sprite");
+		sprite.style.top = parSprite.posPart + "px";
+		sprite.style.left = parSprite.posLarg + "px";
+		
+}
